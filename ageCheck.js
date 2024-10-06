61136c7c44d6041f9cb4ecf86b4d7b566c7e3954
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-10-07 00:39:25 +0800

    If-Statement

---
+++ ageCheck.js
@@ -0,0 +1,10 @@
+let age = prompt("Enter your age:");
+age = parseInt(age);
+
+if (age < 13){
+    console.log("You are a child");
+}if (age < 20){
+    console.log("You are a teenager");
+}if (age > 19){
+    console.log("You are an adult");
+}
