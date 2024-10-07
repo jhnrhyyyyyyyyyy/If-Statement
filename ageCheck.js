ea82fd2339615492cc21745e8f0068fbcad6ea6a
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-10-07 18:48:13 +0800

    If Statement Activity

--- ageCheck.js
+++ ageCheck.js
@@ -1,11 +1,10 @@
 let age = prompt("Enter your age:");
-
 age = parseInt(age);
 
 if (age < 13){
-    console.log("You are a child");
+    console.log("You are a child.");
 }else if (age >= 13 && age < 20){
-    console.log("You are a teenager");
+    console.log("You are a teenager.");
 }else{
-    console.log("You are an adult");
+    console.log("You are an adult.");
 }
